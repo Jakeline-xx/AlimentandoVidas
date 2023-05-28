eb8a556c957d0da26d6a082ca84b5d8b0ec55ee6
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2023-05-28 00:58:52 -0300

    criacao das classes de modelo

---
+++ src/main/java/com/projeto/alimentandovidas/model/AcaoSocial.java
@@ -0,0 +1,44 @@
+package com.projeto.alimentandovidas.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class AcaoSocial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_acao_social")
+    private Long idAcaoSocial;
+
+    @NotEmpty(message = "O campo descricao é obrigatório")
+    @Column(name = "descricao_completa")
+    private String descricaoCompleta;
+
+    @NotEmpty(message = "O campo local é obrigatório")
+    @Column(name = "local")
+    private String local;
+
+    @NotEmpty(message = "O campo data inicio é obrigatório")
+    @Column(name = "data_inicio")
+    private LocalDateTime dataInicio;
+
+    @NotEmpty(message = "O campo data fim é obrigatório")
+    @Column(name = "data_fim")
+    private LocalDateTime dataFim;
+
+    @NotEmpty(message = "é necessário informar o público permitido")
+    @Column(name = "publicoPermitido")
+    private String publicoPermitido;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organizacao_id")
+    @Fetch(FetchMode.JOIN)
+    private Organizacao organizacao;
+
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+}
---
+++ src/main/java/com/projeto/alimentandovidas/model/Organizacao.java
@@ -0,0 +1,72 @@
+package com.projeto.alimentandovidas.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@Builder
+@Table(name = "organizacao")
+public class Organizacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_organizacao")
+    private Long idOrganizacao;
+
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @NotEmpty(message = "O campo nome fantasia é obrigatório")
+    @Column(name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @NotEmpty(message = "O campo estado é obrigatório")
+    @Column(name = "estado")
+    private String estado;
+
+    @NotEmpty(message = "O campo cidade é obrigatório")
+    @Column(name = "cidade")
+    private String cidade;
+
+    @NotEmpty(message = "O campo telefone é obrigatório")
+    @Column(name = "telefone")
+    private String telefone;
+
+//    @Column(name = "email")
+//    private String email;
+//
+//    @Column(name = "senha")
+//    private String senha;
+
+//    @Column(name = "logo")
+//    private String logoCaminho;
+
+    @NotEmpty(message = "O campo descricao é obrigatório")
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "site")
+    private String site;
+
+    @Column(name = "chave_pix")
+    private String chavePix;
+
+    @OneToMany(mappedBy = "organizacao")
+    @Fetch(FetchMode.JOIN)
+    @Column(name = "acao_social")
+    private List<AcaoSocial> acaoSocialList;
+
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+}
