78414886421cf1bcf6d73f3bed341f9035249bfe
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2023-05-27 23:09:39 -0300

    inicio

---
+++ src/main/java/com/projeto/alimentandovidas/AlimentandovidasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.alimentandovidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimentandovidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentandovidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/alimentandovidas/AlimentandovidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.alimentandovidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentandovidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
