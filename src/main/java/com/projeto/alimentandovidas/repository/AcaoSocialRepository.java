809fe285939fb4fe25f152455b3c02d5029f65c5
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2023-05-28 21:50:38 -0300

    criacao dos repositorios

--- src/main/java/com/projeto/alimentandovidas/repository/AcaoSocialRepository.java
+++ src/main/java/com/projeto/alimentandovidas/repository/AcaoSocialRepository.java
@@ -1,4 +1,10 @@
 package com.projeto.alimentandovidas.repository;
 
-public class AcaoSocialRepository {
+import com.projeto.alimentandovidas.model.AcaoSocial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AcaoSocialRepository extends JpaRepository<AcaoSocial, Long> {
+
 }
--- src/main/java/com/projeto/alimentandovidas/repository/OrganizacaoRepository.java
+++ src/main/java/com/projeto/alimentandovidas/repository/OrganizacaoRepository.java
@@ -1,4 +1,10 @@
 package com.projeto.alimentandovidas.repository;
 
-public class OrganizacaoRepository {
+import com.projeto.alimentandovidas.model.Organizacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizacaoRepository extends JpaRepository<Organizacao, Long> {
+
 }
